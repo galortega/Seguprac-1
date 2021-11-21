e7348f07b28a610944d037a2490b3452abdfcd2a
chrisciokler <christianpradociokler@gmail.com>
2021-11-21 18:37:16 -0500

    inicial commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navigator from './src/navigation';
+import { UserStateProvider } from './src/user_context';
+
+const App = () => {
+	return (
+		<UserStateProvider>
+			<Navigator/>
+		</UserStateProvider>
+	);
+};
+
+export default App;
